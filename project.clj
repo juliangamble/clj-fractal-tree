7a8a086e6daaab1202f43c98a2ee1fbe8c2df076
Julian Gamble <mail@juliangamble.com>
2012-01-20 21:47:29 +1100

    Initial code drop

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject clj-fractal-tree "1.0.0-SNAPSHOT"
+  :description "Clojure GUI Fractal Tree"
+
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]                 
+ 	:source-path "src"
+	:main fractaltree.fractaltree
+)
---
+++ sign.cmd
@@ -0,0 +1,1 @@
+jarsigner -keystore compstore -storepass sunshine -keypass sunshine -signedjar clj-fractal-tree.jar clj-fractal-tree-1.0.0-SNAPSHOT-standalone.jar signFiles
